962070305c384333d73be81573d6ed0c2a79213f
Trovoada01 <112417747+Trovoada01@users.noreply.github.com>
2023-02-27 15:07:35 -0300

    Add files via upload

---
+++ nopain/js/scripts.js
@@ -0,0 +1,25 @@
+//declaração de variaveis  //
+
+let nome = 'Eu'
+ let numero_inteiro = 3 
+ let numero_flutuante = 2.12321312
+ let condicao = false
+const nomes = 'você' 
+/*
+console.log(typeof numero_inteiro)
+console.log(typeof numero_flutuante)
+console.log(typeof condicao)
+console.log(typeof nome)
+*/
+
+let n1 = parseInt(prompt('informe um numero'))
+let n2 = parseInt(prompt('informe outro numero'))
+
+let soma = n1+n2
+let sub = n1-n2
+let mult = n1*n2
+let div = n1/n2
+
+alert('a soma é ' + soma + '\n' + 'a subração é ' +sub+ '\n' + 'a multiplicação é ' +mult+ '\n' +
+'a divisão é '+ div)
+  
